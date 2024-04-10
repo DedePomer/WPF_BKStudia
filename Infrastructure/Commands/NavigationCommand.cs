18905bc7c7d2a99714e34e154bebd34663f32539
=DedePomer <=t343st@gmail.com>
2024-04-10 18:37:52 +0300

    Добавил основные  классы

---
+++ Infrastructure/Commands/NavigationCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_BKStudia.Infrastructure.Commands.Base;
+using WPF_SQL.Infrastructure.Navigation;
+using WPF_BKStudia.ViewModel.Base;
+
+
+namespace WPF_BKStudia.Infrastructure.Commands
+{
+    internal class NavigationCommand<VM> : Command where VM : ViewModel.Base.ViewModel
+    {
+        private NavigationStore _navigationStore;
+        private readonly Func<VM> _createViewModel;
+
+        public NavigationCommand(NavigationStore navigationStore, Func<VM> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}
+
---
+++ Infrastructure/Navigation/NavigationStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_BKStudia.ViewModel.Base;
+
+
+namespace WPF_SQL.Infrastructure.Navigation
+{
+    internal class NavigationStore
+    {
+        //?
+        public event Action CurrentViewModelChanged;
+
+        //Здесь хранится текущаяя ViewModel
+        private ViewModel? _currentViewModel;
+        public ViewModel? CurrentViewModel 
+        {             
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        //?
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
