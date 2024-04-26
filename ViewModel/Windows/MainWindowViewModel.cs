81176802c0d5af919e51f22f2780d82361bea884
=DedePomer <=t343st@gmail.com>
2024-04-26 23:32:07 +0300

    Добавил смешную картинку в главном меню

--- ViewModel/Windows/MainWindowViewModel.cs
+++ ViewModel/Windows/MainWindowViewModel.cs
@@ -19,8 +19,8 @@ namespace WPF_BKStudia.ViewModel.Windows
         {
             if (!Directory.Exists("Tests")) Directory.CreateDirectory("Tests");
 
-            //navigationStore.CurrentViewModel = new MenuPageViewModel(navigationStore);           
-            navigationStore.CurrentViewModel = new CreateTestViewModel(navigationStore);
+            navigationStore.CurrentViewModel = new MenuPageViewModel(navigationStore);
+            //navigationStore.CurrentViewModel = new CreateTestViewModel(navigationStore);
             navigationStore.CurrentViewModelChanged += () => OnCurrentViewChanged();
         }
 
