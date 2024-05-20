ee0e74816a535b43dcf0abe43cbb48d832c12b04
=DedePomer <=t343st@gmail.com>
2024-05-20 03:28:05 +0300

    Изменил TextQuestion и работую с интерфейсом TakeTestPageViewModel

--- Infrastructure/Services/FileReader.cs
+++ Infrastructure/Services/FileReader.cs
@@ -31,8 +31,9 @@ namespace WPF_BKStudia.Infrastructure.Services
                 {
                     Id = Int32.Parse(mas[0]),
                     Type = (QuestionEnum)Int32.Parse(mas[1]),
-                    Text = mas[2]
-                });
+                    Text = mas[2],
+                    TrueAnswer = new ObservableCollection<Answer>()
+                }); ;
                 switch (questions[i].Type)
                 {
                     case QuestionEnum.TextQuestion:
--- Model/DataType/TextQuestion.cs
+++ Model/DataType/TextQuestion.cs
@@ -46,6 +46,7 @@ namespace WPF_BKStudia.Model.DataType
                 NotifyPropertyChanged();
             }
         }
+        public ObservableCollection<Answer>? TrueAnswer { get; set; }
         public ObservableCollection<Answer>? ListAnswer { get; set; }
         public SolidColorBrush QuestionColor
         {
