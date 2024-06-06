65c4cd1a60717e84542b4a99b5f2f3085eeb2677
=DedePomer <=t343st@gmail.com>
2024-06-06 23:57:59 +0300

    Доделал TextQuestionToColorConverter

--- Infrastructure/Converters/TextQuestionToColorConverter.cs
+++ Infrastructure/Converters/TextQuestionToColorConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace WPF_BKStudia.Infrastructure.Converters
 {
@@ -7,7 +8,13 @@ namespace WPF_BKStudia.Infrastructure.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            switch ((bool)value)
+            {
+                case true:
+                    return new SolidColorBrush(Colors.LightBlue);
+                case false:
+                    return new SolidColorBrush(Colors.MediumVioletRed);
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
