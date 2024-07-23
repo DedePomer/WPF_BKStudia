7c04c8502b1b97595ec8bf423a7d9ab92d63abd3
Чирков <@DESKTOP-VDON385>
2024-07-23 13:04:49 +0300

    тест

--- Infrastructure/Interfaces/IQuestionAnswer.cs
+++ Infrastructure/Interfaces/IQuestionAnswer.cs
@@ -2,13 +2,7 @@
 {
     public interface IQuestionAnswer
     {
-        /// <summary>
-        /// <typeparam name = "Id">Индификатор вопроса (ответа)</typeparam>
-        /// </summary>
         public int Id { get ; set ; }
-        /// <summary>
-        /// <typeparam name = "Text">Текст вопроса (ответа)</typeparam>
-        /// </summary>
         public string Text { get; set; }
     }
 }
